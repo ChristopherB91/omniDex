1ed418120da169962f93528edaadf901c67aba9e
Chris <chrisbrowne91@gmail.com>
2024-07-23 08:25:53 -0400

    optimized

--- src/components/selections.tsx
+++ src/components/selections.tsx
@@ -2,7 +2,7 @@ interface Props {
   num: number;
 }
 
-const presets = [
+const preset1 = [
   { presetName: "Preset1" },
   { presetName: "Preset2" },
   { presetName: "Preset3" },
@@ -10,22 +10,35 @@ const presets = [
   { presetName: "Preset5" },
   { presetName: "Preset6" },
 ];
+
+const preset2 = [
+  { presetName: "Preset2" },
+  { presetName: "Preset3" },
+  { presetName: "Preset4" },
+  { presetName: "Preset5" },
+  { presetName: "Preset6" },
+  { presetName: "Preset7" },
+];
 const Selections: React.FC<Props> = ({ num }) => {
+  const presets = num !== 6 ? preset1 : preset2;
   return (
     <>
       <ul id="presets">
-        {presets.map((preset, index) => (
-          <li
-            key={index}
-            className={
-              num === index
-                ? "font-custom bg-lime-800 text-white animate-text"
-                : "font-custom animate-text"
-            }
-          >
-            {preset.presetName}
-          </li>
-        ))}
+        {presets.map((preset, index) => {
+          const isActive = num !== 6 ? num === index : num - 1 === index;
+          return (
+            <li
+              key={index}
+              className={
+                isActive
+                  ? "font-custom bg-lime-800 text-white animate-text"
+                  : "font-custom animate-text"
+              }
+            >
+              {preset.presetName}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
