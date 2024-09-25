a277e2fd9f954c9cc14f351079e580d1334e6425
Chris <chrisbrowne91@gmail.com>
2024-09-25 11:19:42 -0400

    hopefully done

--- server/src/main/java/com/chris/Primus/controller/AlienController.java
+++ server/src/main/java/com/chris/Primus/controller/AlienController.java
@@ -3,7 +3,6 @@ package com.chris.Primus.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chris.Primus.model.Alien;
 import com.chris.Primus.service.AlienService;
 
-@CrossOrigin(origins = "http://127.0.0.1:5173")
+@CrossOrigin(origins = "https://omnidex.vercel.app")
 @RestController
 @RequestMapping("/primus/")
 public class AlienController {
